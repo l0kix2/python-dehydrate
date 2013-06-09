ce63a785949684be71289ff2110c19fc4411b6df
l0ki <l0kix2@gmail.com>
2013-06-09 18:46:10 +0400

    Coverage support. Attempt #2.

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 # coding: utf-8
 
+import sys
+
 # It's odd, but setuptools provide by distribute package.
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 setup(
     name='dehydrate',
     version='0.1dev',
     packages=find_packages(),
-    tests_require=['pytest', 'mock', 'pytest-cov', 'coveralls']
+    tests_require=['pytest', 'mock', 'coveralls'],
+    cmdclass={'test': PyTest},
 )
