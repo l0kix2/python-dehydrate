e913a2fe4198f4e46896f786ab63ee9a7cc78f01
l0ki <l0kix2@gmail.com>
2013-06-12 12:31:30 +0400

    Base information in Readme.

---
+++ README.rst
@@ -0,0 +1,38 @@
+python-dehydrate
+================
+
+Small lib for representing python objects as a dicts.
+
+.. image:: https://travis-ci.org/l0kix2/python-dehydrate.png?branch=master
+    :target: https://travis-ci.org/l0kix2/python-dehydrate?branch=master..
+
+.. image:: https://coveralls.io/repos/l0kix2/python-dehydrate/badge.png?branch=master
+    :target: https://coveralls.io/r/l0kix2/python-dehydrate?branch=master
+
+
+Examples
+========
+
+
+Testing
+=======
+Test written with use of `pytest`_ library with neat `pytest pep8 plugin`_.
+You should run ``python setup.py test`` for running full test suite or
+``coverage run --source=dehydrate setup.py test`` for tests with coverage.
+Tests automatically runs at `Travis CI`_.
+
+.. _pytest: http://pytest.org/
+.. _pytest pep8 plugin: https://pypi.python.org/pypi/pytest-pep8
+.. _Travis CI: https://travis-ci.org/l0kix2/python-dehydrate?branch=master
+
+
+Contribution
+============
+Any contribution is welcome. Use fork/pull request mechanism on github.
+
+If you add some code, you should add some tests, so coverage of master branch
+should always be 100%. Refer to Testing_ section for more instructions.
+
+Let me speak from my heart :). I will be very glad, if you correct my clumsy
+english phrases in docs and docstings or even advise more appropriate names
+for variables in code.
--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@
 import sys
 
 # It's odd, but setuptools provide by distribute package.
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
 
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = ['--pep8']
+        self.test_args = [
+            '--pep8',
+            '--clearcache',
+        ]
         self.test_suite = True
 
     def run_tests(self):
@@ -19,11 +22,33 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 
+readme_text = open('README.rst').read()
+changelog_text = open('CHANGELOG.rst').read()
 setup(
     name='dehydrate',
     version='0.1dev',
-    packages=find_packages(),
+    packages=['dehydrate'],
+    url='https://github.com/l0kix2/python-dehydrate',
+    license='MIT',
+    author='Kirill Sibirev',
+    author_email='l0kix2@gmail.com',
+    description='Small lib for representing python objects as a dicts',
+    long_description=readme_text + '\n\n' + changelog_text,
     setup_requires=['six'],
     tests_require=['pytest', 'mock', 'coverage', 'coveralls'],
     cmdclass={'test': PyTest},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
 )
