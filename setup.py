146311f0906e801e0387640211604edd5d4337cd
Kirill Sibirev <sibirev@yandex-team.ru>
2013-10-30 15:01:30 +0400

    Bump version: 0.3.5 → 0.3.6

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ readme_text = open('README.rst').read()
 changelog_text = open('CHANGELOG.rst').read()
 setup(
     name='dehydrate',
-    version='0.3.5',
+    version='0.3.6',
     packages=['dehydrate'],
     url='https://github.com/l0kix2/python-dehydrate',
     license='MIT',
